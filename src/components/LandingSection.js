d7c5cf292a0faf02c6102b4cecc61e84759d0750
jonfontx <Your GitHub Email Address>
2023-07-17 20:19:43 +0100

    fixing video

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -17,7 +17,7 @@ const LandingSection = () => (
    isDarkBackground
    backgroundColor="#2A4365"
  >
-<video className='videoTag' loop muted style={{position:"absolute", zIndex:0}}>
+<video className='videoTag' autoPlay loop muted playsinline style={{position:"absolute", zIndex:0}}>
 <source src={video} type='video/webm' />
 </video>
    <VStack spacing={16} style={{zIndex:1}} >
