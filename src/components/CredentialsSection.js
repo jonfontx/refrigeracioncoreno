459e289dca2d8ba5096c165ab86675ea5f12e8ad
jonfontx <Your GitHub Email Address>
2023-07-21 17:06:21 +0100

    minor fix

--- src/components/CredentialsSection.js
+++ src/components/CredentialsSection.js
@@ -39,7 +39,7 @@ const CredentialsSection = () => {
             <Box id="certificates"
             display="grid"
             gridTemplateColumns="1fr 1fr"
-            gridGap={10}
+            gridGap={60}
             style={{zIndex:1}}
             >
             <HStack className="pdfbox" borderRadius={16} overflow="hidden">
