52873fb69cb907ad6cf5d7f987f37b398d7a492b
jonfontx <Your GitHub Email Address>
2023-07-21 17:15:07 +0100

    minor fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -85,7 +85,7 @@ const Header = () => {
     > 
      <Box color="white" maxWidth="1280px" margin="0 auto"> 
        <HStack 
-         px={16} 
+         px={{base:6, lg:16}} 
          py={4} 
          justifyContent="space-between"
          alignItems="center"
