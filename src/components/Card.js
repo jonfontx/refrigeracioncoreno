7369505b68a284d089898675ebe0a53ea5ed9608
jonfontx <Your GitHub Email Address>
2024-03-08 14:50:08 +0000

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ const Card = ({ title, description, imageSrc}) => {
        cursor="pointer" 
        borderRadius="xl" 
      > 
-       <Image borderRadius="xl" src={imageSrc} alt={title} style={{height:800}}/> 
+       <Image borderRadius="xl" src={imageSrc} alt={title} style={{height:600}}/> 
        <VStack spacing={4} p={4} alignItems="flex-start"> 
          <HStack justifyContent="space-between" alignItems="center"> 
            <Heading as="h3" size="md"> 
